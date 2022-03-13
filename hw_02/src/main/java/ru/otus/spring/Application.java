c0f31309d1e5aa375a2644e3a14a69478fb47f50
Рамиль <bischev@gmail.com>
2022-03-13 18:46:16 +0500

    homework 02, spring bean config. Refactor after code-review #2

--- hw_02/src/main/java/ru/otus/spring/Application.java
+++ hw_02/src/main/java/ru/otus/spring/Application.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import ru.otus.spring.utils.printer.ExamPrinter;
-import ru.otus.spring.utils.printer.ExamPrinterImpl;
 
 @Configuration
 @ComponentScan
@@ -14,7 +13,7 @@ public class Application {
 
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Application.class);
-        final ExamPrinter examPrinter = context.getBean(ExamPrinterImpl.class);
+        final ExamPrinter examPrinter = context.getBean(ExamPrinter.class);
         examPrinter.print();
     }
 }
