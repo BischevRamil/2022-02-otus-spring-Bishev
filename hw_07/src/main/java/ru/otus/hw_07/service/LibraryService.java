07e5688bd542bd792072ea94abb1cca2c2cdd27c
Бишев Рамиль Раисович <rbishev@cinimex.ru>
2022-11-20 19:18:58 +0300

    homework_07 Spring data jpa. Правки по замечаниям.

--- hw_07/src/main/java/ru/otus/hw_07/service/LibraryService.java
+++ hw_07/src/main/java/ru/otus/hw_07/service/LibraryService.java
@@ -1,112 +1,34 @@
 package ru.otus.hw_07.service;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-import ru.otus.hw_07.exception.AuthorNotFoundException;
-import ru.otus.hw_07.exception.BookNotFoundException;
-import ru.otus.hw_07.exception.CommentNotFoundException;
-import ru.otus.hw_07.exception.GenreNotFoundException;
 import ru.otus.hw_07.model.Author;
 import ru.otus.hw_07.model.Book;
-import ru.otus.hw_07.model.Comment;
 import ru.otus.hw_07.model.Genre;
-import ru.otus.hw_07.repository.AuthorRepository;
-import ru.otus.hw_07.repository.BookRepository;
-import ru.otus.hw_07.repository.CommentRepository;
-import ru.otus.hw_07.repository.GenreRepository;
 
 import java.util.List;
 
-@Service
-@RequiredArgsConstructor
-public class LibraryService {
+public interface LibraryService {
 
+    Long getBooksCount();
 
-    private final BookRepository bookRepository;
+    void newBookSave(String title, String comment, String authorName, String genreName);
 
-    private final AuthorRepository authorRepository;
+    Book findBookById(long id);
 
-    private final GenreRepository genreRepository;
+    List<Book> findAllBooks();
 
-    private final CommentRepository commentRepository;
+    List<Author> findAllAuthors();
 
-    public Long getBooksCount() {
-        return bookRepository.count();
-    }
+    List<Genre> findAllGenres();
 
-    @Transactional
-    public void newBookSave(String title, String comment, String authorName, String genreName) {
-        var author = authorRepository
-                .findByName(authorName)
-                .orElseThrow(() -> new AuthorNotFoundException(String.format("Author with name '%s' not found", authorName)));
-        var genre = genreRepository
-                .findByName(genreName)
-                .orElseThrow(() -> new GenreNotFoundException(String.format("Author with name '%s' not found", genreName)));
-        var book = new Book();
-        book.setTitle(title);
-        book.getComments().add(new Comment(comment));
-        book.setAuthor(author);
-        book.setGenre(genre);
-        bookRepository.save(book);
-    }
+    List<Book> findAllBooksByAuthor(String name);
 
-    public Book findBookById(long id) {
-        return bookRepository
-                .findById(id)
-                .orElseThrow(() -> new BookNotFoundException(String.format("Book with id = %s not found", id)));
-    }
+    void updateBookTitleById(long id, String title);
 
-    public List<Book> findAllBooks() {
-        return bookRepository.findAll();
-    }
+    void addCommentById(long id, String comment);
 
-    public List<Author> findAllAuthors() {
-        return authorRepository.findAll();
-    }
+    void updateCommentById(long id, String comment);
 
-    public List<Genre> findAllGenres() {
-        return genreRepository.findAll();
-    }
+    void deleteCommentById(long id);
 
-    @Transactional(readOnly = true)
-    public List<Book> findAllBooksByAuthor(String name) {
-        return authorRepository.findByName(name).orElseThrow().getBooks();
-    }
-
-    @Transactional
-    public void updateBookTitleById(long id, String title) {
-        var book = bookRepository
-                .findById(id)
-                .orElseThrow(() -> new BookNotFoundException(String.format("Book with id = %s not found", id)));
-        book.setTitle(title);
-        bookRepository.save(book);
-    }
-
-    @Transactional
-    public void addCommentById(long id, String comment) {
-        var book = bookRepository
-                .findById(id)
-                .orElseThrow(() -> new BookNotFoundException(String.format("Book with id = %s not found", id)));
-        book.getComments().add(new Comment(comment));
-        bookRepository.save(book);
-    }
-
-    @Transactional
-    public void updateCommentById(long id, String comment) {
-        var commentEntity = commentRepository
-                .findById(id)
-                .orElseThrow(() -> new CommentNotFoundException(String.format("Comment with id = %s not found", id)));
-        commentEntity.setComment(comment);
-        commentRepository.save(commentEntity);
-    }
-
-    @Transactional
-    public void deleteCommentById(long id) {
-        commentRepository.deleteById(id);
-    }
-
-    public void bookDeleteById(long id) {
-        bookRepository.deleteById(id);
-    }
+    void bookDeleteById(long id);
 }
